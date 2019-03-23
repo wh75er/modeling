f6caed3d3a08e5b7e03b443531f17e8ea8068582
kam16u810 <bokseng12@gmail.com>
2019-03-23 10:18:56 +0300

    remove print

--- lab_01/picard.py
+++ lab_01/picard.py
@@ -1,23 +1,5 @@
 import sympy as sp
 
-class equation():
-    def __init__(self, pow_x=None, pow_y=None):
-        self.pow_x = 1
-        self.pow_y = 1
-        if pow_x != None and pow_x != "":
-            self.pow_x = pow_x
-        if pow_y != None and pow_x != "":
-            self.pow_y = pow_y
-
-        self.equation = "x**" + str(pow_x) + " + y**" + str(pow_y) 
-
-    def get_pow_x(self):
-        return self.pow_x
-
-    def get_pow_y(self):
-        return self.pow_y
-    
-
 def picard(x0, v0, n, eq):
 #   U'(x) = f(x, u);
 #       x0 < x < xr;
@@ -39,17 +21,5 @@ def picard(x0, v0, n, eq):
         y = v0 + sp.integrate(pow(x, pow_x) + pow(y, pow_y) , (x, x0, x))
         print(y)
         
-    print()
     return y
-        
-
-
-if __name__ == "__main__":
-    print()
-    eq = equation(int(input("Input pow of x: ")), int(input("Input pow of y: ")))
-    approximation = (int(input("Input approximation value: ")))
-
-    res = picard(0, 0, approximation, eq)
-
-    y = lambda x: eval(str(res))
-    print(y(2))
+ 
