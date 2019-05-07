ebe5b13548ec3e129067886c2998d895e3399e1b
kam16u810 <bokseng12@gmail.com>
2019-05-07 19:37:53 +0300

    add collocation method + graphic drawing

--- lab_03/calculate.py
+++
@@ -1,7 +0,0 @@
-import data as d
-
-def alpha(x, a, b):
-    return d.a_alpha/(x-b_alpha)
-
-def k(x, a, b):
-    return d.ak/(x-d.bk)
---
+++ lab_03/functions.py
@@ -0,0 +1,50 @@
+from math import exp, log
+import data as d
+
+def alpha(x):
+    return d.a_alpha/(x-d.b_alpha)
+
+def k(x):
+    return d.ak/(x-d.bk)
+
+"""
+ ____            _
+| __ )  __ _ ___(_)___
+|  _ \ / _` / __| / __|
+| |_) | (_| \__ \ \__ \
+|____/ \__,_|___/_|___/
+
+
+"""
+
+k1 = lambda x : log(d.F0/k(x))
+k2 = lambda x : d.F0 * (k(x) - alpha(x))/(k(x) * alpha(x) * exp(d.l)) + d.Tenv
+
+def u0(x):
+    return exp(k1(x) - x) + k2(x)
+def u0I(x):
+    return -exp(k1(x) - x)
+def u0II(x):
+    return exp(k1(x) - x)
+
+def u1(x):
+    return (x**2 - d.l**2)**2
+def u1I(x):
+    return 4*x**3 - 4*x*d.l**2
+def u1II(x):
+    return 12*x**2 - 4*d.l**2
+
+def u2(x):
+    return (x**3 - d.l**3)**3
+def u2I(x):
+    return 9*x**8 - 18*x**5*d.l**3 + 9*x**2*d.l**6
+def u2II(x):
+    return 72*x**7 - 90*x**4*d.l**3 + 18*x*d.l**6
+
+def u3(x):
+    return (x**4 - d.l**4)**4
+def u3I(x):
+    return 16*x**15 - 48*x**11*d.l**4 + 48*x**7*d.l**8 - d.l**12
+def u3II(x):
+    return 240*x**14 - 528*x**10*d.l**4 + 336*x**6*d.l**8
+
